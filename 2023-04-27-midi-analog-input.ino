f492e9c9a9f058aee28bd8065d74ef906ad404af
deniz <github@ledeniz.de>
2023-04-27 22:27:06 +0200

    first commit

---
+++ 2023-04-27-midi-analog-input.ino
@@ -0,0 +1,72 @@
+#include "MIDIUSB.h"
+
+const int INPUT_PIN = A0;
+const byte MIDI_CHANNEL = 0;
+const byte MIDI_CC = 74;
+
+int sensor_value = 0;
+int old_sensor_value = 0; 
+
+int t = 0;
+int old_t = 0;
+
+const int numReadings = 10;
+
+int readings[numReadings];  // the readings from the analog input
+int readIndex = 0;          // the index of the current reading
+int total = 0;              // the running total
+int average = 0;            // the average
+
+void setup() {
+  for (int thisReading = 0; thisReading < numReadings; thisReading++) {
+    readings[thisReading] = 0;
+  }
+
+  readSensor();
+  old_sensor_value = sensor_value;
+}
+
+void loop() {
+  t = millis();
+  int delta = t - old_t;
+
+  readSensor();
+
+  if (sensor_value != old_sensor_value) {
+    sendValue(sensor_value);
+    old_sensor_value = sensor_value;
+  }
+}
+
+void sendValue(int value) {
+  //Serial.println(value);
+
+  byte channel = MIDI_CHANNEL;
+  byte control = MIDI_CC;
+
+  midiEventPacket_t event = {0x0B, 0xB0 | channel, control, (byte) value};
+  MidiUSB.sendMIDI(event);
+  MidiUSB.flush();
+}
+
+void readSensor() {
+    // subtract the last reading:
+  total = total - readings[readIndex];
+  // read from the sensor:
+  readings[readIndex] = analogRead(INPUT_PIN);
+  // add the reading to the total:
+  total = total + readings[readIndex];
+  // advance to the next position in the array:
+  readIndex = readIndex + 1;
+
+  // if we're at the end of the array...
+  if (readIndex >= numReadings) {
+    // ...wrap around to the beginning:
+    readIndex = 0;
+  }
+
+  // calculate the average:
+  average = total / numReadings;
+
+  sensor_value = map(average, 0, 1023, 127, 0);
+}
